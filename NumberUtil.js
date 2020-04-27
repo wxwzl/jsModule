6298bacb41252614d985647c0e38bb06f44fb7a2
wxwzl <zgkj456@163.com>
2020-04-27 23:15:38 +0800

    getMulti优化版

--- NumberUtil.js
+++ NumberUtil.js
@@ -4,12 +4,13 @@ function NumberUtil() {
 NumberUtil.prototype = {
     // 给定两个字符串，返回它们的乘法结果。例如getMulti('2', '3')，函数的输出为'6'。
     getMulti: function (a, b) {
-        const maxlen = 1;
+        const maxlen = 5;
+
         function strToNumberArray(a) {
             let aArray = [];
             while (a.length >= maxlen) {
-                aArray.push(Number(a.substr(0, maxlen)));
-                a = a.substr(maxlen);
+                aArray.push(Number(a.substr(a.length - maxlen)));
+                a = a.substr(0, a.length - maxlen);
             }
             if (a != "") {
                 aArray.push(Number(a));
@@ -25,15 +26,15 @@ NumberUtil.prototype = {
         let left = 0;
         let numi = 0;
         let numj = 0;
-        for (let i = lenA - 1; i >= 0; i--) {
-            for (let j = lenB - 1; j >= 0; j--) {
+        for (let i = 0; i < lenA; i++) {
+            for (let j = 0; j < lenB; j++) {
                 numi = aNumArray[i];
                 numj = bNumArray[j];
                 // console.log(numi, numj);
                 temp = numj * numi;
                 // console.log(numj, "*", numi, "=", temp);
                 temp += left;
-                let strLen = (lenA - i - 1 + lenB - j - 1) * maxlen;
+                let strLen = (i + j) * maxlen;
                 // console.log("位数：", strLen);
                 if (result.length > strLen) {
                     let right = result.substr(0, result.length - strLen);
@@ -41,7 +42,8 @@ NumberUtil.prototype = {
                     right = Number(right);
                     temp += right;
                 } else {
-                    for (let n = 0; n < strLen - result.length; n++) {
+                    let zeroLen = strLen - result.length;
+                    for (let n = 0; n < zeroLen; n++) {
                         result = "0" + result;
                     }
                 }
